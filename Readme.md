bb8a12f08a58d774bb019fcfa3fd6b1d094b38c1
Dan <danroal@ciencias.unam.mx>
2025-09-19 04:24:04 -0600

    Initial comit factorial y mcd

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# MathUtils
+
+Esta es una librería para herramientas de uso común de matemáticas. La idea es ir ampliando con el tiempo más y más funciones.
+
+## Funciones soportadas
+- Factorial de un número natural
+- Mayor común divisor de dos números
---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+/// Returns the factorial of a number.
+/// # Examples
+/// ```
+/// let result = math_utils::factorial(5);
+/// assert_eq!(result, 120);
+/// ```
+pub fn factorial(n: u64) -> u64 {
+    (1..=n).product()
+}
+
+/// Returns the maximum common divisor of two numbers.
+/// # Examples
+/// ```
+/// let result = math_utils::max_com_div(12, 15);
+/// assert_eq!(result, 3);
+/// ```
+pub fn max_com_div(a: u64, b: u64) -> u64 {
+    let mut divisors_a = Vec::new();
+    let mut divisors_b = Vec::new();
+    for i in 1..=a{
+        let rem = a % i;
+        match rem {
+            0 => divisors_a.push(i),
+            _ => continue,
+        }
+    }
+    for i in 1..=b{
+        let rem = b % i;
+        match rem {
+            0 => divisors_b.push(i),
+            _ => continue,
+        }
+    }
+    let mut common_divisors = Vec::new();
+    for i in divisors_a {
+        if divisors_b.contains(&i) {
+            common_divisors.push(i);
+        }
+    }
+    *common_divisors.iter().max().unwrap()
+}
